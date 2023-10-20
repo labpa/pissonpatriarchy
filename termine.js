6be807e3ebba50e0f4323650e6e31b285e513d3f
Pascal <pbienenstein@posteo.de>
2023-10-20 18:12:33 +0200

    try to create a navbar in js

--- termine.js
+++ termine.js
@@ -1,6 +1,18 @@
-console.log("JS is running from termine.js");
+// var divMenu = document.getElementById( "menu" );
+// //create element <ul> to add to div menu.
+// var ulMenu = document.createElement('ul');
+// ulMenu.setAttribute('id', 'nav');
+// var liItem = document.createElement('li');
+// liItem.setAttribute('class', 'current');
 
-const app = document.getElementById("App")
+// ulMenu.appendChild(liItem);
 
-app.innerHTML="<p>Hallo in p</p>"
-console.log(app)
+// var href = document.createElement('a');
+// href.setAttribute('href', '/sytematic_inventory/views/administrador/index.jsp');
+
+// liItem.appendChild(href);
+
+// href.appendChild(document.createTextNode('Startseite'));
+
+// // Ditto for the other liItem
+// document.getElementById('menu').appendChild(ulMenu);
